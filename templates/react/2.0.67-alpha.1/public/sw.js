614c8d92d8fa0d0957ec01d39fdb64da4bcc55d0
Mac <mackinleymorrone@gmail.com>
2026-07-01 16:57:26 -0400

    Delete templates/react/2.0.67-alpha.1 directory

--- templates/react/2.0.67-alpha.1/public/sw.js
+++
@@ -1,7 +0,0 @@
-importScripts("/controller/controller.sw.js");
-
-addEventListener("fetch", (e) => {
-  if ($scramjetController.shouldRoute(e)) {
-    e.respondWith($scramjetController.route(e));
-  }
-});
